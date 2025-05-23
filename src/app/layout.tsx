7cd298a4871dd84f8bff7133794453982beb9ad7
WooGi1020 <jungwk1020@gmail.com>
2025-05-24 00:47:41 +0900

    begin: 시작

---
+++ src/app/layout.tsx
@@ -0,0 +1,32 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "VeganFree",
+  description: "알레르기 필터링으로 맞춤 비건 음식점을 찾아보세요!",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="ko">
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+        <main>{children}</main>
+      </body>
+    </html>
+  );
+}
