dd3145d991f9f1d18210bb64ee01db5da949b21c
WooGi1020 <jungwk1020@gmail.com>
2025-05-25 16:21:14 +0900

    feat: 헤더 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "../styles/globals.css";
+import Header from "@/components/header/Header";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -25,6 +26,7 @@ export default function RootLayout({
   return (
     <html lang="ko">
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+        <Header />
         <main>{children}</main>
       </body>
     </html>
