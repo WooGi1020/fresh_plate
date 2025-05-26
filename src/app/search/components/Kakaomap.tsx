500b23a693d9ee7b9d893a056fde77c5e4b2d75d
WooGi1020 <jungwk1020@gmail.com>
2025-05-26 21:05:04 +0900

    feat: 카카오맵 설정 및 사용

---
+++ src/app/search/components/Kakaomap.tsx
@@ -0,0 +1,43 @@
+"use client";
+
+import { Map, useKakaoLoader } from "react-kakao-maps-sdk";
+import rawData from "@/data/filtered_active_vegan_restaurants.json";
+import { Restaurant } from "vegan";
+import { useState } from "react";
+import MapMarkerWithPan from "@/app/search/components/MapMarkerWithPan";
+
+const data = rawData as Restaurant[];
+
+export default function KakaoMap() {
+  const [loading, error] = useKakaoLoader({
+    appkey: process.env.NEXT_PUBLIC_KAKAO_JAVASCRIPT_KEY!,
+    libraries: ["services", "clusterer"],
+  });
+
+  const [selectedId, setSelectedId] = useState<string | null>(null);
+
+  if (loading) return <div>Loading...</div>;
+  if (error) return <div>Error loading KakaoMap</div>;
+
+  return (
+    <Map
+      center={{ lat: 37.56693232422167, lng: 126.97866354704028 }}
+      style={{ width: "100%", height: "100%" }}
+      level={5}
+      onClick={() => setSelectedId(null)}
+    >
+      {data.map((restaurant: Restaurant) => {
+        const lat = Number(restaurant.lat);
+        const lng = Number(restaurant.lng);
+        return (
+          <MapMarkerWithPan
+            key={restaurant.id}
+            restaurant={restaurant}
+            selectedId={selectedId}
+            setSelectedId={setSelectedId}
+          />
+        );
+      })}
+    </Map>
+  );
+}
--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -16,28 +16,28 @@ const Header = () => {
     <header
       className={`fixed inset-0 w-full h-[70px] flex justify-between items-center px-7 z-50 ${
         pathname == "/" ? "bg-[#FFFFFF]" : "bg-[#EAEEDB]"
-      }`}
+      } shadow-md`}
     >
       <Link href="/" className="relative w-[88px] h-[65px]">
         <Image src="/images/home/small_logo.png" alt="홈 로고 링크 이미지" fill className="object-contain" />
       </Link>
-      <div className="flex justify-evenly items-center max-w-[600px] w-full min-w-[300px]">
-        <Link href="/search" className="flex items-center gap-1">
+      <div className="flex justify-evenly items-center max-w-[600px] w-full min-w-[200px]">
+        <Link href="/search" className="flex items-center gap-1 p-1 hover:bg-[#504840]/10 rounded-md">
           <SearchIcon />
-          <span className="text-[20px]">식당 찾기</span>
+          <span className="text-[20px] sm:text-[20px] hidden sm:inline ">식당 찾기</span>
         </Link>
-        <Link href="/search" className="flex items-center gap-1">
+        <Link href="/search" className="flex items-center gap-1 p-1 hover:bg-[#504840]/10 rounded-md">
           <EventIcon />
-          <span className="text-[20px]">My 혜택</span>
+          <span className="text-[20px] sm:text-[20px] hidden sm:inline ">My 혜택</span>
         </Link>
-        <Link href="/search" className="flex items-center gap-1">
+        <Link href="/search" className="flex items-center p-1 gap-1 hover:bg-[#504840]/10 rounded-md">
           <CenterIcon />
-          <span className="text-[20px]">고객 센터</span>
+          <span className="text-[20px] sm:text-[20px] hidden sm:inline ">고객 센터</span>
         </Link>
       </div>
       <Link href="/login" className="flex items-center gap-1">
         <LoginIcon />
-        <span className="text-[20px]">Login</span>
+        <span className="text-[20px] sm:text-[20px] hidden sm:inline">Login</span>
       </Link>
     </header>
   );
