9a36bbf5dc02671f2ea4cefafcd3a8ed8e39f885
WooGi1020 <jungwk1020@gmail.com>
2025-05-29 18:10:21 +0900

    feat: 사이드바 등 주요 컴포넌트 추가

---
+++ src/app/search/components/customBalloon/CustomBalloon.tsx
@@ -0,0 +1,119 @@
+import VeganIcon from "@/icons/vegan_icon.svg";
+import AllegyIcon from "@/icons/allegy_icon.svg";
+import GlutenIcon from "@/icons/gluten_free_icon.svg";
+import CloseIcon from "@/icons/close_icon.svg";
+import { Restaurant } from "vegan";
+import StarRating from "@/app/search/components/customBalloon/StarRating";
+import { useEffect, useState } from "react";
+
+const CustomBalloon = ({
+  restaurant,
+  onClose,
+  map,
+}: {
+  restaurant: Restaurant;
+  onClose: () => void;
+  map: kakao.maps.Map;
+}) => {
+  const [placeUrl, setPlaceUrl] = useState<string | null>(null);
+
+  useEffect(() => {
+    if (!map) return;
+    const ps = new kakao.maps.services.Places();
+
+    ps.keywordSearch(`${restaurant.name}`, (data, status) => {
+      if (status === kakao.maps.services.Status.OK) {
+        setPlaceUrl(data[0].place_url);
+      } else {
+        setPlaceUrl(null);
+      }
+    });
+  }, [map]);
+
+  return (
+    <div
+      className="w-[350px] sm:w-[420px] shadow-lg  p-4 text-neutral-900 speech-bubble cursor-default opacity-90"
+      onWheel={(e) => {
+        e.stopPropagation();
+      }}
+    >
+      {/* 닫기 버튼 */}
+      <button
+        className="absolute top-2 right-2 text-2xl font-bold text-neutral-900 border-2 border-transparent hover:border-neutral-900/50 rounded-md p-1"
+        onClick={onClose}
+      >
+        <CloseIcon width={20} height={20} />
+      </button>
+
+      {/* 상단: 이미지 + 식당 정보 */}
+      <div className="flex gap-4">
+        <div className="w-24 h-24 bg-gray-300 flex items-center justify-center rounded-md text-[#6b4f3b] text-xl">
+          photo
+        </div>
+        <div className="flex-1">
+          <a
+            href={`${placeUrl}`}
+            target="_blank"
+            className="text-xl font-semibold mb-1 truncate max-w-[180px] sm:max-w-[250px] hover:underline"
+            title={restaurant.name}
+          >
+            {restaurant.name}
+          </a>
+
+          <div className="flex items-center gap-2 mb-1">
+            <StarRating rating={3.5} />
+            <span className="text-md text-neutral-900 mb-1">3.5</span>
+            <span className="text-md text-neutral-900 mb-1">(4)</span>
+          </div>
+          <div className="flex gap-2">
+            {/* 아이콘은 임시로 원으로 대체 */}
+
+            <div title="비건 표시">
+              <VeganIcon width={30} height={30} />
+            </div>
+            {restaurant.menu.includes("글루텐프리".trim()) && (
+              <div title="글루텐프리 표시">
+                <GlutenIcon width={30} height={30} />
+              </div>
+            )}
+            <div title="알러지 반응 표시">
+              <AllegyIcon width={30} height={30} fill="#85A947" />
+            </div>
+          </div>
+        </div>
+      </div>
+
+      {/* 구분선 */}
+      <div className="flex items-center w-full gap-4 my-2">
+        <hr className="flex-grow border-[1.5px] border-neutral-900" />
+        <p className="whitespace-nowrap text-neutral-900 font-semibold">Review</p>
+        <hr className="flex-grow border-[1.5px] border-neutral-900" />
+      </div>
+      {/* 알러지 반응 구분 */}
+      <div className="flex justify-between mb-3 gap-4 w-full text-center">
+        <div className="border border-neutral-900 px-3 py-1 rounded-lg text-sm">
+          <span>
+            알레르기 <br /> 반응 없음
+          </span>
+        </div>
+        <div className="border border-neutral-900 px-3 py-1 rounded-lg text-sm">
+          <span>
+            알레르기 <br /> 반응 주의
+          </span>
+        </div>
+        <div className="bg-secondary-default px-3 py-1 rounded-lg text-sm shadow-inner border border-neutral-900">
+          <span>
+            알레르기 <br /> 반응 있음
+          </span>
+        </div>
+      </div>
+
+      {/* 리뷰 */}
+      <div className="text-sm py-2 flex items-center justify-center flex-col h-[65px] overflow-y-scroll">
+        <p className="text-neutral-900/50">현재 리뷰가 없습니다.</p>
+      </div>
+    </div>
+  );
+};
+
+export default CustomBalloon;
---
+++ src/app/search/components/customBalloon/StarRating.tsx
@@ -0,0 +1,25 @@
+import EmptyStarIcon from "@/icons/empty_star_icon.svg";
+import FilledStarIcon from "@/icons/filled_star_icon.svg";
+
+const StarRating = ({ rating }: { rating: number }) => {
+  const stars = Array.from({ length: 5 }, (_, i) => {
+    const fill = Math.min(Math.max(rating - i, 0), 1); // 0 ~ 1
+    return (
+      <div key={i} className="relative w-6 h-6">
+        {/* 빈 별 */}
+        <EmptyStarIcon fill="none" stroke="#CBD2A9" strokeWidth={1.5} className="absolute top-0 left-0 w-full h-full" />
+
+        {/* 채워진 별 */}
+        <FilledStarIcon
+          fill="#CBD2A9"
+          className="absolute top-0 left-0 h-full"
+          style={{ width: `${fill * 100}%`, overflow: "hidden" }}
+        />
+      </div>
+    );
+  });
+
+  return <div className="flex space-x-1 items-center w-fit">{stars}</div>;
+};
+
+export default StarRating;
