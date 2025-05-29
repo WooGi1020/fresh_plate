c74fb8fd2d576459f160be53228fabc220a3ced8
우기 <113000290+WooGi1020@users.noreply.github.com>
2025-05-30 00:43:30 +0900

    Feat: customballoon 이미지 삽입 (#10)
    * chore: 필요 사진 추가
    
    * feat: customBalloon 이미지 추가

---
+++ src/constants/image_render_list.ts
@@ -0,0 +1,8 @@
+const imageRenderList = [
+  "/images/balloon/china.jpg",
+  "/images/balloon/korea.jpg",
+  "/images/balloon/japan.jpg",
+  "/images/balloon/italy.jpg",
+];
+
+export default imageRenderList;
