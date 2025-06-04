e28d33a3954fc62edac1f47b34017f8c284ad8da
WooGi1020 <jungwk1020@gmail.com>
2025-06-05 00:57:02 +0900

    feat: 상수 파일 추가

---
+++ src/constants/allergyFilterMap.ts
@@ -0,0 +1,22 @@
+const allergyFilterMap = {
+  no_egg: "계란",
+  no_milk: "우유",
+  no_buckwheat: "메밀",
+  no_peanut: "땅콩",
+  no_soy: "대두",
+  no_wheat: "밀",
+  no_mackerel: "고등어",
+  no_crab: "게",
+  no_shrimp: "새우",
+  no_squid: "오징어",
+  no_oyster: "굴",
+  no_abalone: "전복",
+  no_mussel: "홍합",
+  no_walnut: "호두",
+  no_almond: "아몬드",
+  no_pinenut: "잣",
+  no_peach: "복숭아",
+  no_tomato: "토마토",
+} as const;
+
+export default allergyFilterMap;
