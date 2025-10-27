a1d758cfbcb3249b420267244618dcce26692091
우기 <jungwk1020@gmail.com>
2025-10-27 19:16:30 +0900

    feat: 미들웨어 로직 작성

--- src/libs/api/auth.api.ts
+++ src/libs/api/auth.api.ts
@@ -2,12 +2,21 @@ import { SigninValues, SignupValues } from "@/types/auth.schema";
 import apiClient from "./apiClient";
 import { useAuthStore } from "@/store/useAuthStore";
 
+const setCookie = (name: string, value: string, days = 1) => {
+  const expires = new Date();
+  expires.setTime(expires.getTime() + days * 24 * 60 * 60 * 1000);
+  document.cookie = `${name}=${value}; Path=/; Expires=${expires.toUTCString()}; SameSite=Lax`;
+};
+
 export const login = async (data: SigninValues) => {
   const res = await apiClient.post("/api/auth/login", data);
   const { accessToken, refreshToken, memberId, nickname } = res.data.data;
   useAuthStore
     .getState()
     .login({ nickname, memberId }, { accessToken, refreshToken });
+
+  setCookie("accessToken", accessToken);
+  setCookie("refreshToken", refreshToken);
 };
 
 export const register = async (data: SignupValues) => {
@@ -16,5 +25,9 @@ export const register = async (data: SignupValues) => {
   useAuthStore
     .getState()
     .login({ nickname, memberId }, { accessToken, refreshToken });
+
+  setCookie("accessToken", accessToken);
+  setCookie("refreshToken", refreshToken);
+  setCookie("onboardingAllowed ", "true");
   return res.data;
 };
---
+++ src/middleware.ts
@@ -0,0 +1,21 @@
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+
+export function middleware(req: NextRequest) {
+  const accessToken = req.cookies.get("accessToken")?.value;
+  const onboardingAllowed = req.cookies.get("onboardingAllowed")?.value;
+
+  if (!accessToken && req.nextUrl.pathname.startsWith("/my-page")) {
+    return NextResponse.redirect(new URL("/sign", req.url));
+  }
+
+  if (
+    req.nextUrl.pathname.startsWith("/onboarding") &&
+    onboardingAllowed !== "true"
+  ) {
+    // 1회 허용이 아니면 리다이렉트
+    return NextResponse.redirect(new URL("/", req.url));
+  }
+
+  return NextResponse.next();
+}
